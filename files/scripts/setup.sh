15afca09deae1c9084a865e2729c7fb262488616
jbanuls <jbanuls@gmail.com>
2025-05-01 19:25:03 +0200

    2025-05-01T19:25:03,472967146+02:00

---
+++ files/scripts/setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+# set -oue pipefail
+set -ouex pipefail
+
+# Your code goes here.
+# echo 'This is an example shell script'
+# echo 'Scripts here will run during build if specified in recipe.yml'
+echo 'Setup Script'
+
+# Starship Shell Prompt
+#curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+#tar -xzf /tmp/starship.tar.gz -C /tmp
+#install -c -m 0755 /tmp/starship /usr/bin
+# shellcheck disable=SC2016
+echo 'eval "$(starship init bash)"' >> /etc/bashrc
+
+# cockpit and extensions
+#rpm-ostree install fastfetch
+#echo 'fastfetch' >> /etc/profile
+
+echo 'export $PATH:/usr/share/setup-scripts' >> /etc/profile
+
+
+echo Done
