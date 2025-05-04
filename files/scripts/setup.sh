f92fcf7ad9cd486f36c03545aefb65f19b146d21
jbanuls <jbc@nobody.com>
2025-05-04 19:20:05 +0200

    2025-05-04T19:20:05,097895239+02:00

--- files/scripts/setup.sh
+++ files/scripts/setup.sh
@@ -12,18 +12,18 @@ set -ouex pipefail
 echo 'Setup Script'
 
 # Starship Shell Prompt
-#curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
-#tar -xzf /tmp/starship.tar.gz -C /tmp
-#install -c -m 0755 /tmp/starship /usr/bin
-# shellcheck disable=SC2016
+curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+tar -xzf /tmp/starship.tar.gz -C /tmp
+install -c -m 0755 /tmp/starship /usr/bin
+ shellcheck disable=SC2016
 echo 'eval "$(starship init bash)"' >> /etc/bashrc
 echo 'export PATH=$PATH:/usr/share/setup-scripts' >> /etc/bashrc
 chmod a+x /usr/share/setup-scripts/sysctl-manager
 #echo 'uname -a' >> /etc/bashrc
 
 # cockpit and extensions
-#rpm-ostree install fastfetch
-#echo 'fastfetch' >> /etc/profile
+rpm-ostree install fastfetch
+#echo 'fastfetch' >> /etc/bashrc
 
 
 echo Done
